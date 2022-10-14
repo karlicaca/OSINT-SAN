27f0344629a37051bdcb1c68d3c83b328cc9e609
Karli <89438902+karlicaca@users.noreply.github.com>
2022-10-15 05:59:48 +0700

    Update osintsan.py

--- osintsan.py
+++ osintsan.py
@@ -10,21 +10,21 @@ from module.utils.ban import page_start
 # Вам необходимо использовать свои API для максимальной анонимности и защиты переваемых данных.
 
 api = {
-    "Shodan": "",
+    "Shodan": "K09VkSIHkNrPZuCzJD0xwHQpkQyvvcb4",
     # Ваш личный token Shodan, вписывать между "сюда_вписывать_ваш_токен"  Получать токен в -->> https://www.shodan.io/
 
-    "Gmap_g": "",
+    "Gmap_g": "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",
     # Ваш личный token google, вписывать между "сюда_вписывать_ваш_токен" Получать токен в -->>
     # https://console.cloud.google.com/google/maps-apis/
 
-    "IP_api": "",  # API Массового сканирование ip address "сюда_вписывать_ваш_токен"
+    "IP_api": "K09VkSIHkNrPZuCzJD0xwHQpkQyvvcb4",  # API Массового сканирование ip address "сюда_вписывать_ваш_токен"
     # Получать токен в -->> https://ipapi.com/
 
     "ngrok.set_auth_token": "",
     # big brother 13,  "сюда_вписывать_ваш_токен" # Получать токен в -->>
     # https://dashboard.ngrok.com/get-started/your-authtoken
 
-    "virustotal_api": "",
+    "virustotal_api": "f2db8f66c178028151969aee7e00094803669c159ce5dfcd4fb3b186a03809d6",
     # Выше впиши API вирус тотал
     "cms_detect_api": "",
     # Ниже пример как нужно указывать API
