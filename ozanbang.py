7831f663e0965b5f1d8a69fbf60739a25ca457d8
Karli <89438902+karlicaca@users.noreply.github.com>
2022-10-15 06:26:47 +0700

    Update and rename osintsan.py to ozanbang.py

--- ozanbang.py
+++ ozanbang.py
@@ -20,13 +20,13 @@ api = {
     "IP_api": "K09VkSIHkNrPZuCzJD0xwHQpkQyvvcb4",  # API Массового сканирование ip address "сюда_вписывать_ваш_токен"
     # Получать токен в -->> https://ipapi.com/
 
-    "ngrok.set_auth_token": "",
+    "ngrok.set_auth_token": "1x4e1bUVDcjsaIQSio1OmAVidRj_6MoL3Fmj2VgMz45sxZxhq",
     # big brother 13,  "сюда_вписывать_ваш_токен" # Получать токен в -->>
     # https://dashboard.ngrok.com/get-started/your-authtoken
 
     "virustotal_api": "f2db8f66c178028151969aee7e00094803669c159ce5dfcd4fb3b186a03809d6",
     # Выше впиши API вирус тотал
-    "cms_detect_api": "",
+    "cms_detect_api": "20ec4173-84b9-4555-a3f8-2595103f4d37",
     # Ниже пример как нужно указывать API
     "phone_apis": "80fd4720becad81c5d60f5364c7d4b2f", # API номер телефона phone_apis -- https://numverify.com/dashboard?logged_in=1
 
